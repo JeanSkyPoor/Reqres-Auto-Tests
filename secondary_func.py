16873b0af94215b5898735d9e41f2f32698ef548
Jean_Sky_Poor <katovano2705@gmail.com>
2022-02-01 13:11:13 +0300

    Update import

--- secondary_func.py
+++ secondary_func.py
@@ -1,2 +1,12 @@
+import json
+
 def check_code_response(response, status_code) -> None:
     assert status_code == response.status_code
+
+def read_and_return_data(name_func):
+    """
+    Return correct data for func from data.json file
+    """
+    with open("data.json", 'r') as f:
+        data = json.load(f)
+        return data[name_func][0]

a1f5afad1ecde65f466177dff5385b25775e53ac
Jean_Sky_Poor <katovano2705@gmail.com>
2022-02-02 20:18:07 +0300

    idk

--- test_pytest.py
+++ test_pytest.py
@@ -1,10 +1,12 @@
 import requests
 from secondary_func import *
 from env_variables import *
+import allure
+import pytest
 
 
-
-
+@pytest.mark.regression
+@allure.severity(allure.severity_level.MINOR)
 def test_get_list_users():
     response = requests.get(f'{BASE_URL}api/users?page=2')
 
@@ -14,6 +16,8 @@ def test_get_list_users():
     assert len(response['data']) == 6
 
 
+@pytest.mark.regression
+@allure.severity(allure.severity_level.CRITICAL)
 def test_get_single_user():
     response = requests.get(f'{BASE_URL}api/users/2')
 
@@ -25,12 +29,16 @@ def test_get_single_user():
     assert response['data']['last_name'] == 'Weaver'
 
 
-def test_get_sigle_user_not_found():
+@pytest.mark.regression
+@allure.severity(allure.severity_level.MINOR)
+def test_get_single_user_not_found():
     response = requests.get(f'{BASE_URL}api/users/23')
 
     check_code_response(response, 404)
 
 
+@pytest.mark.regression
+@allure.severity(allure.severity_level.MINOR)
 def test_post_create():
     response = requests.post(f'{BASE_URL}api/users', data=read_and_return_data('post_create', DATA_REQUEST_FILE_NAME))
 
@@ -42,6 +50,8 @@ def test_post_create():
     assert len(response) == 4
 
 
+@pytest.mark.regression
+@allure.severity(allure.severity_level.MINOR)
 def test_put_update():
     response = requests.put(f'{BASE_URL}api/users/2', data=read_and_return_data('put_update', DATA_REQUEST_FILE_NAME))
 
@@ -51,14 +61,19 @@ def test_put_update():
     assert response['job'] == 'zion resident'
 
 
+@pytest.mark.regression
+@allure.severity(allure.severity_level.MINOR)
 def test_delete_user():
     response = requests.delete(f'{BASE_URL}api/users/2')
 
     check_code_response(response, 204)
 
 
+@pytest.mark.regression
+@allure.severity(allure.severity_level.MINOR)
 def test_post_register_successful():
-    response = requests.post(f'{BASE_URL}api/register', data=read_and_return_data('post_register_successful', DATA_REQUEST_FILE_NAME))
+    response = requests.post(f'{BASE_URL}api/register',
+                             data=read_and_return_data('post_register_successful', DATA_REQUEST_FILE_NAME))
 
     check_code_response(response, 200)
     response = response.json()
@@ -66,8 +81,11 @@ def test_post_register_successful():
     assert response['id'] is not None
 
 
+@pytest.mark.regression
+@allure.severity(allure.severity_level.MINOR)
 def test_post_register_unsuccessful():
-    response = requests.post(f'{BASE_URL}api/register', data=read_and_return_data('post_register_unsuccessful', DATA_REQUEST_FILE_NAME))
+    response = requests.post(f'{BASE_URL}api/register',
+                             data=read_and_return_data('post_register_unsuccessful', DATA_REQUEST_FILE_NAME))
 
     check_code_response(response, 400)
     response = response.json()
@@ -75,17 +93,23 @@ def test_post_register_unsuccessful():
     assert response['error'] == 'Missing password'
 
 
+@pytest.mark.regression
+@allure.severity(allure.severity_level.MINOR)
 def test_post_login_successful():
-    response = requests.post(f'{BASE_URL}api/login', data=read_and_return_data('post_login_successful', DATA_REQUEST_FILE_NAME))
-                                                                        
+    response = requests.post(f'{BASE_URL}api/login',
+                             data=read_and_return_data('post_login_successful', DATA_REQUEST_FILE_NAME))
+
     check_code_response(response, 200)
     response = response.json()
 
     assert response['token'] is not None
 
 
+@pytest.mark.regression
+@allure.severity(allure.severity_level.MINOR)
 def test_post_login_unsuccessful():
-    response = requests.post(f'{BASE_URL}api/login', data=read_and_return_data('post_login_unsuccessful', DATA_REQUEST_FILE_NAME))
+    response = requests.post(f'{BASE_URL}api/login',
+                             data=read_and_return_data('post_login_unsuccessful', DATA_REQUEST_FILE_NAME))
     check_code_response(response, 400)
     response = response.json()
 
